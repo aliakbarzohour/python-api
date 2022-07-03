0bfd89fbcb576d20c0898101909b2534e0eb7141
aliakbarzohour <aliakbar.zohour@gmail.com>
2022-07-03 18:08:34 +0430

    it's a better

--- api.py
+++ api.py
@@ -6,7 +6,8 @@ response = requests.get(url)
 # get the data from the response
 data = response.json()
 # giving the user wanted to find
-input = input("What do you want to know about the user? : ")
+options = '\n*** id - name - username - website - email - address - phone - company *** \n'
+input = input(f"{options}\n--- What do you want to know about the user? : ")
 # giving the all json data 
 for i in data:
     # loop in the data
